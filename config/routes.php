6c1557d2a088be92adaccac1c3e34ca87325bc8e
Mike Landau <mikeland86@gmail.com>
2015-06-06 17:23:27 +0200

    Added README instructions to test sample app

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 return [
   // default route. You can rmeove if you don't want one.
   '__default__' => ['welcome', 'index'],
+  // '__default__' => ['landing', 'index'], // uncomment this for sample app
 
   // Custom regex routes, "#" not allowed. Don't end with slash if
   // you want non slash to match (or use /?). Use capture groups for arguments:
