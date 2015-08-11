810dafb1d5c9722c6452c4a6e35cb21cce444628
Mike Landau <mikeland86@gmail.com>
2015-08-11 14:58:11 +0200

    Fix async handler

--- graphp/core/GPAsyncControllerHandler.php
+++ graphp/core/GPAsyncControllerHandler.php
@@ -2,14 +2,19 @@
 
 class GPAsyncControllerHandler extends GPURIControllerHandler {
 
+  private $handled = false;
+
   public function handle($method, array $args) {
+    $this->handled = true;
     $uri = parent::handle($method, $args);
     $log = ini_get('error_log') ?: '/dev/null';
     execx('php '.ROOT_PATH.'public/index.php %s >> '.$log.' 2>&1 &', $uri);
   }
 
   public function __destruct() {
-    $this->handle('', []);
+    if (!$this->handled) {
+      $this->handle('', []);
+    }
   }
 
 }
