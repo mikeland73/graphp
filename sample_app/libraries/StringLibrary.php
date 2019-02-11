9233449fb814cd9e2979f7ef28baaffc9a5d3d34
codefactor-io <support@codefactor.io>
2019-02-11 06:16:22 +0000

    [CodeFactor] Apply fixes

--- sample_app/libraries/StringLibrary.php
+++ sample_app/libraries/StringLibrary.php
@@ -4,7 +4,7 @@ final class StringLibrary extends GPObject {
 
   public static function truncate($string, $length) {
     if (mb_strlen($string) > $length) {
-      $string = mb_substr($string, 0, $length-3).'...';
+      $string = mb_substr($string, 0, $length - 3).'...';
     }
     return $string;
   }
